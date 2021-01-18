c60925e7518b2f448511951fc810df427a71a4e4
jbrasko <jbrasko0508@gmail.com>
2021-01-18 16:30:23 -0600

    add bands index, new, show

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -4,9 +4,30 @@ class BandsController < ApplicationController
         @bands = current_manager.bands
     end
 
+    def show
+        @band = current_manager.bands.find_by(id: params[:id])
+        if !@band
+            redirect_to manager_path(current_manager)
+        end
+    end
+
+    def new
+        @band = Band.new
+    end
+
+    def create
+        @band = current_manager.bands.build(band_params)
+
+        if @band.save
+            redirect_to bands_path
+        else
+            render :new
+        end
+    end
+
     private
 
     def band_params
-        params.require(:band).permit(:title, :description)
+        params.require(:band).permit(:name, :manager_id)
     end
 end
--- app/models/band.rb
+++ app/models/band.rb
@@ -2,4 +2,6 @@ class Band < ApplicationRecord
   belongs_to :manager
   has_many :band_members
   has_many :members, through: :band_members
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
