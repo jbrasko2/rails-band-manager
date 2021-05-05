03b6f37c07842327edd121ba3d560ddb019c1cf6
jbrasko <jbrasko0508@gmail.com>
2021-05-05 13:27:51 -0700

    add client options to omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+    provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
+    client_options: {
+      site: 'https://graph.facebook.com/v3.0',  # this is the example API version
+      authorize_url: "https://www.facebook.com/v3.0/dialog/oauth"
+    }
   end 
